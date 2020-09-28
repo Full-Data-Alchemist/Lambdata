f70463461db249c3ef9f4f98b451ecddf03a4f70
Full_Data_Alchemist <imani.beasley.72@gmail.com>
2020-09-28 12:49:31 -0500

    second commit

---
+++ Lambdata/ExMod.py
@@ -0,0 +1,12 @@
+""""Lambdata - a Collection of Data Science Helpper Functions"""
+
+import pandas as pd
+import numpy as np
+
+Favorite_Animals = ['cat', 'dog', 'kiwi','mongoose', 'lion']
+
+def df_cleaner(df):
+    """Cleans df"""
+    # TODO - implement df cleaner
+
+
