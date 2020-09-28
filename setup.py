a91a7dd980f5f010a236742bfbb57076cb927063
Full_Data_Alchemist <imani.beasley.72@gmail.com>
2020-09-28 13:00:03 -0500

    third commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+""""Lambdata - a Collection of Data Science Helpper Functions"""
+
+import setuptools
+
+REQUIRED = [
+    "numpy",
+    "pandas"
+]
+
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+
+setuptools.setup(
+    name = "Lamdata-alchemy",
+    version = "0.0.1",
+    author = "Full_Data_alchemist"
+    
+)
